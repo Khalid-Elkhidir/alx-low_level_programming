f4ba5d36617e66eec9fff54047e7bb4c1ff8c042
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-13 16:39:08 -0700

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/*This program prints a text using puts function*/
+/**
+ * This program prints a text using puts function
+ */
 
-int main(void)
+int main (void)
 {
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return(0);
 }
