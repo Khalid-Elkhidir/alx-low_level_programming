7a1175ec7adabbeda51254d4c5ce586b467e3373
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-14 16:21:29 -0700

    Signle digit combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints single digit
+ * combinations
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 9 ; i++)
+	{
+		putchar('0' + i);
+		putchar(44);
+		putchar(32);
+	}
+
+	putchar('0' + ++i);
+
+	return (0);
+}
