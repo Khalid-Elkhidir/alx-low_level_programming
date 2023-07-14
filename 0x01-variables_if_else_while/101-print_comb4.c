3f805118bc2b0f8c6e9863f7d0f95d8b30e7537a
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-14 16:39:01 -0700

    Three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints two digits
+ * combinations
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (j = 1 ; j < 9 ; j++)
+		{
+			for (k = 2 ; k < 10 ; k++)
+			{
+				if (i != j && i < j)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					if (i != 7)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
