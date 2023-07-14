fc9c7feae2775215a97bbcbc625be4ee4463e60d
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-14 15:38:23 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints alphabet in lowercase
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for(ch = 'a' ; ch <= 'z' : ch++) 
+	{
+		putchar(ch);
+	}
+	return (0);
+}
