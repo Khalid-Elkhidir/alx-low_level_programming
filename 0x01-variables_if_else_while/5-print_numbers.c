187f11921092e88bb92ba7d7d43ca916b041d852
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-14 15:54:42 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints numbers 0-9
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; ch < 10 ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
