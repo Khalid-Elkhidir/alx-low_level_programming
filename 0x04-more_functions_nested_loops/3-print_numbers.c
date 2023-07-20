c87e6154996ddf99aa91e9eaf5ca605b9cc2c9e4
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-20 09:58:28 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9 with a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		-putchar("\n");
+	}
+}
