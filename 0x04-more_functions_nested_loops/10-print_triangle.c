4ac887cc5dbf8db97ee3e1521739eecfeb509d5b
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-20 12:24:44 -0700

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,25 @@
 
 void print_tringle(int size)
 {
-	int i, j;
-
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-
-		for (j = 0; j < (size - i); j++)
-			_putchar(' ');
-
-		for (j = 0; j <= i; j++)
-			_putchar('#');
-
 		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
+		for (i = 0; i < size; i++)
+		{
+
+			for (j = 0; j < (size - i); j++)
+				_putchar(' ');
+
+			for (j = 0; j <= i; j++)
+				_putchar('#');
+
+			_putchar('\n');
+
+		}
 	}
 }
