b1a8436651c9a1e5cce3480a787f9df5c71056da
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-20 09:37:32 -0700

    Is_upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,9 +3,9 @@
 /*
  * _isupper - checks if character is uppercase
  * @c: is character to be checked
+ *
  * Return: 1 if character is uppercase, otherwise 0
  */
-
 int _isupper(int c)
 {
 	return ((c >= 'A' || c <= 'Z'));
