2d1afae4e78d4c708e0f7bd1028ef904a31ae98a
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-20 10:55:03 -0700

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straint line
+ *@n: number of times '_' is printed 
+ *
+ *Return: void
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+		_putchar('_');
+	_putchar('\n');
+}
