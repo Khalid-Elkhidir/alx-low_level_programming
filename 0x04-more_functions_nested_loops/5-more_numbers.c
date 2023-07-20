69d56d0bacaf61540d5d33098d4fc792ee246366
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-20 10:31:30 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j
+
+	i = '0';
+
+	while (i <= '9')
+	{
+		j = '0';
+
+		_putchar(i);
+
+		if (i == '9')
+		{
+			int k;
+
+			k = '1';
+			
+			while (j <= '4')
+			{
+
+				_putchar(k);
+				_putchar(j);
+				j++;
+			{
+		}
+
+		i++;
+	}
+		_putchar('\n');
+}
