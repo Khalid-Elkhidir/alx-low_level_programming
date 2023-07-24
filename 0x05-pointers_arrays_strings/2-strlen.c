8910161f62e1a86377f92e88076ea9734cfde24d
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-24 15:53:03 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - returns string length
+ * @s: pointer to char
+ *
+ *Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	char c;
+
+	length = 0;
+
+	while (*s != '/0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
