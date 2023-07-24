70f6ec52087c4cf22531f1dc5880ae1132c90da3
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-24 16:48:04 -0700

    Puts Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ *@s: pointer to char
+ *
+ *Return: void
+ */
+
+void print_rev(char *s)
+{
+	char *i;
+	int length;
+
+	length = _strlen(s);
+	i = str;
+
+	while (length >= 0)
+	{
+		i = (s + length);
+		_putchar(*i);
+		length--;
+	}
+
+	_putchar('\n');
+}
