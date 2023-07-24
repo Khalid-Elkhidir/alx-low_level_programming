dbb416e12bcb300c1bcf8375ce970a11d14edcf0
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-24 16:32:44 -0700

    Puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
  * _puts - prints a string followed by a new line
@@ -10,13 +10,15 @@
 void _puts(char *str)
 {
 	int i;
+	int index;
 
 	i = *str;
+	index = 1;
 
 	while (i != 0)
 	{
 		_putchar(i);
-		i = *(str + 1);
+		i = *(str + index);
 	}
 
 	_putchar('\n');
