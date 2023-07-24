c10a6f302bbb2eda96d3d781355dc0a85d88f038
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-24 15:30:24 -0700

    Swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - change value of variable to 98
+ *@a: pointer to first integer
+ *@b: pointer to second integer
+ *
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i;
+	i = *a;
+	a* = *b;
+	*b = i;
+}
