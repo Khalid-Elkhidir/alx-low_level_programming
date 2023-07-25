ac785e44f8f3d5ed323276713553c66159ed1834
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-24 17:42:51 -0700

    Puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char followed by new line
+ *@str: pointer to char
+ *
+ *Return: void
+ */
+
+void _puts(char *str)
+{
+	char *i;
+
+	i = str;
+
+	while (*i != 0)
+	{
+		_putchar(*i);
+		i = i + 2;
+	}
+
+	_putchar('\n');
+}
