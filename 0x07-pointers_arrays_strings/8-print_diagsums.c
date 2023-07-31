7007209afd0d558a25aa4369ad2328f207f881b5
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-07-31 15:43:48 -0700

    Diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,8 +17,10 @@ void print_diagsums(int *a, int size)
 
 	for (i ; i < size; i++)
 	{
+/*
 		sum1 += *(a + ((size * i) + i));
 		sum2 += *(a + ((size * i) + size - 1 - i));
+*/
 	}
 	printf("%d, ", sum1);
 	printf("%d\n", sum2);
