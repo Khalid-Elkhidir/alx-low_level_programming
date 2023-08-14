343c49be603363e3316e71d661b00434680ee85b
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-08-14 12:04:04 -0700

    Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+/**
+ * struct dog - Defining a dog structure
+ * @*name: Dog name pointer
+ * @age: Dog age
+ * @*owner: Dog owner pointer
+ *
+ * Description: This structure defines a dog comprised of its name,
+ * 		age and owner
+ */
 struct dog 
 {
 	char *name;
