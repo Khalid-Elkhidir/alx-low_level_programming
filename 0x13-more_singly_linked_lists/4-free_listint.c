c5fe430ca6bebdb27c83323b69ab6212008ad3da
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-08-29 16:32:54 -0700

    Free List

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	if (head->next != NULL)
+	{
+		free_listint(head->next);
+	}
+
+	head = NULL;
+
+}
