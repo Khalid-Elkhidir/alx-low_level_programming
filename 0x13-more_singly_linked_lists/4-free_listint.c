8cbefaf6dc150ffa3f47dba26546020ebe270d1a
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-08-29 16:36:21 -0700

    Free List

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_listint - frees a listint_t list
+ *
+ * @head: the head of the list
+ *
+ * Return: NULL if error
+ */
+
 void free_listint(listint_t *head)
 {
 	if (head == NULL)
