faa0f9c7b49d5f36bc5ca1f62880962f00416832
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-08-29 17:13:51 -0700

    Get Node At Index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (index == 0)
 	{
-		return head;
+		return (head);
 	}
 
 	return (get_nodeint_at_index(head->next, --index));
