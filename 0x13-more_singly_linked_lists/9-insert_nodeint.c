887e41890c4ada3e92d768ba28a27ed01fa78c61
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-08-29 17:35:26 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,28 @@
 #include "lists.h"
 
 /**
+  * get_nodeint_at_index - the nth node of a listint_t linked list.
+  * @head: pointer of the first node
+  * @index: index is the index of the node
+  *
+  * Return: pointer of Index
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	if (index == 0)
+	{
+		return (head);
+	}
+
+	return (get_nodeint_at_index(head->next, --index));
+}
+
+/**
  * insert_nodeint_at_index - inserting a new node at agiven postion
  * @head: double pointer
  * @idx: index of the node
