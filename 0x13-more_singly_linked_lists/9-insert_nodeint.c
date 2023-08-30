9ee227ec090874fcab7e7d82128f049148da9b10
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-08-29 18:04:24 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *node;
 
-	if (*head == NULL && idx != 0)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
