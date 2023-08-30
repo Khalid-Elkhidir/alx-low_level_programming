52f79e786fd6f7ce7ec3d4fdbd479dc916ac0ec0
Khalid Elkhidir <elkhidir.khalid@gmail.com>
2023-08-29 18:12:30 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,6 +42,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
+		if (*head == NULL)
+		{
+			*head->n = n;
+			*head->next = NULL;
+			return (*head);
+		}
+
 		node = malloc(sizeof(listint_t));
 		node->n = n;
 		node->next = *head;
